68f450e3bc99a6042960b0d1a036fbaeb69092c7
Elad Efrat <elad@iNNU.ORG>
2015-05-03 17:02:27 +0300

    Make this a module.

--- malver.js
+++ malver.js
@@ -1,71 +1,14 @@
-var google = require('google'),
-    async = require('async'),
-    _ = require('underscore');
+var malver = require('./');
 
-google.resultsPerPage = 25;
-
-var SAMPLE_SIZE = 20;
-var MALWORDS = ['malware', 'remove', 'virus', 'infect', 'junk', 'spy', 'spam', 'phish'];
-
-function get_words(s) {
-  return _.map(s.split(' '), function (word) { return word.toLowerCase(); });
-}
-
-function has_malword(word) {
-  return !!_.find(MALWORDS, function (malword) {
-    return word.indexOf(malword) !== -1;
-  });  
-}
-
-function is_malwareish(link) {
-  var words = get_words(link.title).concat(get_words(link.description));
-  return !!_.find(words, has_malword);
-}
-
-function analyze(url, callback) {
-  var count = 0,
-      num_malwareish = 0;
-  return async.whilst(
-    function () { return count < SAMPLE_SIZE; },
-    function (callback) {
-      return google(url, function (err, next, links){
-        if (err) {
-          return callback(err);
-        }
-
-        _.detect(links, function (link) {
-          if (is_malwareish(link)) {
-            num_malwareish++;
-          }
-
-          if (++count >= SAMPLE_SIZE) {
-            next = callback;
-            return true;
-          }
-        });
-
-        return next();
-      });
-    },
-    function (err) {
-      var percent_malwareish = (num_malwareish / SAMPLE_SIZE) * 100;
-
-      return callback(err, percent_malwareish);
-    }
-  );
-}
-
-function main() {
-  var url = process.argv[2];
-
-  return analyze(url, function (err, percent_malwareish) {
+function main(url) {
+  return malver.analyze(url, function (err, percent_malwareish) {
     if (err) {
       console.log('Failed');
       return;
     }
 
-    console.log(url, 'is', percent_malwareish + '% malwareish');
+    console.log(url, 'is', percent_malwareish + '% malware-ish');
   });
 }
 
-main();
+main(process.argv[2]);
